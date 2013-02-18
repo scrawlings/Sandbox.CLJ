847261b29d2312ba8d003240ae2c89ef7124e685
Julian Kelsey <julian.b.kelsey@gmail.com>
2013-02-18 16:45:09 +0000

    initial graph function, including a random graph generator and a connectedness test, (also some utilities)

---
+++ graphs.clj
@@ -0,0 +1,61 @@
+(def sample-graph-connected
+	{:nodes #{:a1 :a2 :b1 :b2},
+	 :edges #{[:a1 :b1] [:a1 :b2] [:a1 :a2]} })
+(def sample-graph-disjoint
+	{:nodes #{:a1 :a2 :b1 :b2},
+	 :edges #{[:a1 :b1] [:a1 :b2]} })
+
+(defn add-node [graph node] 
+	(assoc graph :nodes (conj (:nodes graph) node)) )
+
+(defn add-edge [graph edge]
+	(assoc 
+		(reduce add-node graph edge) 
+			:edges (conj (:edges graph) edge)) )
+
+(defn add-edge-bi-di [graph [a b]]
+	(add-edge (add-edge graph [a b]) [b a]) )
+
+
+
+(defn random-pair [source] [(rand-nth source) (rand-nth source)])
+(defn random-edges [nodes]
+	(let [nodes-vector (vec nodes)]
+		(filter (fn [[a b]] (not (= a b)))
+			(repeatedly 
+				#(random-pair nodes-vector) ))))
+
+(defn pair-with [xs x] (map #(vector % x) xs))
+(defn pair-wise [bounded infinite] (mapcat #(pair-with bounded %) infinite))
+(defn seq-to-str [xs] (reduce #(str %1 %2) "" xs))
+(defn labels [] (map (comp keyword seq-to-str) (pair-wise "abcdefg" (iterate inc 0))))
+
+(defn factorial [n] (reduce * (range 1 (inc n))))
+
+(defn random-graph [n k]
+	(let [nodes (set (take n (labels)))
+	      edges (set (take (* k (factorial (dec n))) (random-edges nodes))) ]
+	     {:nodes nodes, :edges edges}))
+
+
+(defn unmarked [graph] (assoc graph :marks #{}))
+
+(require 'clojure.set)
+(defn unmarked-node [graph]
+	(first (clojure.set/difference (:nodes graph) (:marks graph))))
+
+(defn following-nodes [graph node]
+	(set (map second (filter (fn [[a b]] (= a node)) (:edges graph)))))
+
+(defn make-non-directed [graph]
+	(reduce add-edge graph (map (fn [[a b]] [b a]) (:edges graph))))
+
+(defn connected 
+	([graph] (connected (make-non-directed graph) #{} (conj (clojure.lang.PersistentQueue/EMPTY) (first (:nodes graph)))))
+	([graph marks queue]
+		(cond 
+			(empty? queue) (empty? (clojure.set/difference (set (:nodes graph)) marks))
+			:else (let [current-node (peek queue)
+						next-marks (conj marks current-node)
+						following-queue (into (pop queue) (clojure.set/difference (following-nodes graph current-node) next-marks))]
+				(connected graph next-marks following-queue)))))
